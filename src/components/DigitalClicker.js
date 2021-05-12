f66b6bbfefee8276af7dc2bf8fb0195c6133d656
MOBOLA MORONTO <t2wycemo@gmail.com>
2021-05-12 20:22:08 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -19,7 +19,7 @@ class DigitalClicker extends React.Component {
 
   render() {
     return (
-      <button onClick={this.handleClick}>Clickez Ici!</button>
+      <button onClick={this.handleClick}>{this.state.timesClicked}</button>
       );
   }  
 }
