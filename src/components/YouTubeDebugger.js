0fb3b305cc412d9dd4c459cf7bd524bbb27a3e89
MOBOLA MORONTO <t2wycemo@gmail.com>
2021-05-12 19:27:30 +0000

    Automatically backed up by Learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,26 +1,10 @@
 import React from 'react';
 
 class YouTubeDebugger extends React.Component {
-  constructor() {
-    super();
-    
-    this.state = {
-      timesClicked: 0
-    };
-  }
-  
-  handleClick = () => {
-    this.setState(previousState => {
-      return {
-        timesClicked: previousState.timesClicked + 1
-      }
-    })
-  }
+  constructor() {}
 
   render() {
-    return (
-      <button onClick={this.handleClick}>Clickez Ici!</button>
-      );
+    return ();
   }  
 }
 
