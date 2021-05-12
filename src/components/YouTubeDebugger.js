b5e0bf6850219c1aa9cb00f21431045cb3c81e5b
MOBOLA MORONTO <t2wycemo@gmail.com>
2021-05-12 19:26:37 +0000

    Automatically backed up by Learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,27 @@
-// Code YouTubeDebugger Component Here
+import React from 'react';
+
+class YouTubeDebugger extends React.Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      timesClicked: 0
+    };
+  }
+  
+  handleClick = () => {
+    this.setState(previousState => {
+      return {
+        timesClicked: previousState.timesClicked + 1
+      }
+    })
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>Clickez Ici!</button>
+      );
+  }  
+}
+
+export default YouTubeDebugger;
